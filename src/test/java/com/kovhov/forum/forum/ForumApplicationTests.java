272bf3924aba885bf2a6fbb559b83ae79e31d22c
Alexandr <Alexandr>
2023-01-13 14:36:17 +0300

    start

---
+++ src/test/java/com/kovhov/forum/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovhov.forum.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
